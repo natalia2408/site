a6139fd257c2a4c8d7c28e451e30e3600b0e5732
Shepel Natalia <shepel_natalia@list.ru>
2021-03-23 16:00:10 +0300

    change css

--- js/tabs.js
+++ js/tabs.js
@@ -1,14 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
+  console.log(document.querySelectorAll('.how_we_work__steps__link'))
   document.querySelectorAll('.how_we_work__steps__link').forEach(function(tabsBtn) {
     tabsBtn.addEventListener('click', function(event) {
       const path = event.currentTarget.dataset.path
 
-      document.querySelectorAll('.tab-content').forEach(function(tabContent)
+      document.querySelectorAll('.tab__content').forEach(function(tabContent)
       {
         tabContent.classList.remove('tab-content-active')
       })
       document.querySelector(`[data-target="${path}"]`).classList.add('tab-content-active')
-    })
+      })
   })
 })
 
